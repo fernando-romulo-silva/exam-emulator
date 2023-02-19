6827927c45eb9b40ca260e4b600775fc03f1011f
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2023-02-19 17:38:50 +0000

    bla bla

--- src/main/java/org/examemulator/Main.java
+++ src/main/java/org/examemulator/Main.java
@@ -5,7 +5,7 @@ import org.examemulator.gui.exam.ExamController;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
 public class Main {
-
+    
     public static void main(final String... args) {
 
 	final var container = SeContainerInitializer.newInstance().initialize();
@@ -15,14 +15,15 @@ public class Main {
 
 	final var examController = container.select(ExamController.class).get();
 	examController.show();
-
+	
 	Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-	    // log.info("starting shutdown");
+	    
 	    // Both OpenWebBeans and Weld seem to shutdown on their own.
 	    // Weld even prints a stack trace if we close it.
-            // container.close();
-            // log.info("finished shutdown");
+	    if (container.isRunning()) {
+		container.close();
+	    }
+	    
 	}));
-
     }
 }
